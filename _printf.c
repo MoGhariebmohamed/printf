8f374de311690f52c1d095713aaf3c9b0a6ec139
rania-fathallah <87832654+rania-fathallah@users.noreply.github.com>
2023-09-17 16:44:49 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,13 +1,13 @@
 #include <stdarg.h>
 #include <unistd.h>
-/**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
- */
 
-#include <stdarg.h>
-#include <unistd.h>
+
+/**
+* _printf - is a function that do the same functions as print.
+*@format: identifier to look for.
+*
+*Return:  the length of the string.
+*/
 
 int _printf(const char *format, ...) {
     va_list args;
