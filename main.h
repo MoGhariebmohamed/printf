ed9243545bb586c1621e928cb8d97ce8849f8528
rania-fathallah <87832654+rania-fathallah@users.noreply.github.com>
2023-09-17 22:35:54 +0100

    Update main.h

--- main.h
+++ main.h
@@ -10,6 +10,7 @@
 /* Function prototypes */
 int char_printf(int count, char buffer[], int *buffer_index, char c);
 int string_printf(int count, char buffer[], int *buffer_index, const char *str);
+int int_printf(int count, char buffer[], int *buffer_index, int num)
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
