080efa5942bbc864405065de1918d827a6825237
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-18 11:36:34 -0700

    v6 print

--- _printf.c
+++ _printf.c
@@ -19,7 +19,8 @@ int _printf(const char *format, ...) {
 
     if (format == NULL || (format[0] == '%' && format[1] == '\0'))
         return (-1);
-
+    if (format[0] == '%' && format[1] == ' ' && format[2] == '\0')
+		return (-1);
     va_start(args, format);
 
     while (*format) {
