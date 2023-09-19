7fcf7ff180368134183cf5329bfb7b44b71b8a4c
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-19 11:37:42 -0700

    v3 repair

--- _printf.c
+++ _printf.c
@@ -14,8 +14,8 @@ int _printf(const char *format, ...)
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(args, format);
-	 if (format[0] == '%' && format[1] == ' ' && format[2] == '\0')
-		 return (-1);
+/**	 if (format[0] == '%' && format[1] == ' ' && format[2] == '\0')
+		 return (-1);*/
 	while (*format)
 	{
 		if (*format == '%')
