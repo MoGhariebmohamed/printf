339bf061f26955553e44646c080c4d87ce324153
rania-fathallah <87832654+rania-fathallah@users.noreply.github.com>
2023-09-19 23:33:12 +0100

    Update int_printf.c

--- int_printf.c
+++ int_printf.c
@@ -16,40 +16,14 @@ int int_printf(int count, char buffer[], int *buffer_index, va_list args)
 {
     int num = va_arg(args, int);
     char num_buffer[12];
-    int is_negative = 0;
-    int num_length = 0;
-    int i, j;
-    char temp;
+    int num_length = snprintf(num_buffer, sizeof(num_buffer), "%d", num);
 
-    if (num < 0)
-    {
-        is_negative = 1;
-        num = -num;
-    }
-
-    do
-    {
-        num_buffer[num_length++] = '0' + (num % 10);
-        num /= 10;
-    } while (num > 0);
-
-    if (is_negative)
-    {
-        num_buffer[num_length++] = '-';
-    }
-
-    for (i = 0, j = num_length - 1; i < j; i++, j--)
-    {
-        temp = num_buffer[i];
-        num_buffer[i] = num_buffer[j];
-        num_buffer[j] = temp;
-    }
-
-    for (i = 0; i < num_length; i++)
-    {
+    for (int i = 0; i < num_length; i++) {
         buffer[(*buffer_index)++] = num_buffer[i];
         count++;
     }
 
     return count;
+}
+
 }
